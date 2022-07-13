40d746c1e2437a02dbbfe31e558d918bd7f44b97
hashly <ilya12-141998@mail.ru>
2022-07-13 13:01:25 +0300

    remove trash

--- project/src/Server.cpp
+++
@@ -1,7 +0,0 @@
-#include "../inc/Server.hpp"
-
-Server::Server(): _path_config("default_config") {}
-
-Server::Server(std::string path_config): _path_config(path_config) {}
-
-Server::~Server() {}
--- project/src/main.cpp
+++ project/src/main.cpp
@@ -1,21 +1,6 @@
-// #include "../inc/Socket/ListeningSocket.hpp"
-
-// int main() {
-// 	std::cout << "Starting..." << std::endl;
-
-// 	std::cout << "Binding Socket..." << std::endl;
-// 	FT::BindingSocket bs = FT::BindingSocket(AF_INET, SOCK_STREAM, 0, 8080, INADDR_ANY);
-
-// 	std::cout << "Listening Socket..." << std::endl;
-// 	FT::ListeningSocket ls = FT::ListeningSocket(AF_INET, SOCK_STREAM, 0, 3002, INADDR_ANY, 10);
-
-// 	std::cout << "Success" << std::endl;
-// }
-
 #include <iostream>
 
 #include "../inc/Server/SimpleHTTPServer.hpp"
-
 
 int main()
 {
