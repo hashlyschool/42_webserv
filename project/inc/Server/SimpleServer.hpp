dabea0092fa314f4c67b8452f4c0b30914e3f3c5
hashly <ilya12-141998@mail.ru>
2022-07-14 21:10:25 +0300

    add parser and processing signal

--- project/inc/Server/SimpleServer.hpp
+++ project/inc/Server/SimpleServer.hpp
@@ -1,25 +1,31 @@
 #include <stdio.h>
+#include <signal.h>
 #include "../Socket/ListeningSocket.hpp"
 
 namespace FT {
 
-  class SimpleServer {
-  private:
-    ListeningSocket *listeningSocket;
-    bool keeprunning;
+class SimpleServer
+{
+	private:
+		ListeningSocket *listeningSocket;
+		bool keeprunning;
 
-    // Define pure virtual functions, which makes this class abstract
-    virtual void accepter() = 0;
-    virtual void handler() = 0;
-    virtual void responder() = 0;
+		// Define pure virtual functions, which makes this class abstract
+		virtual void	parseConfig();
+		virtual void	accepter() = 0;
+		virtual void	handler() = 0;
+		virtual void	responder() = 0;
 
-  public:
-    SimpleServer(int domain, int service, int protocol, int port, u_long interface, int backlog);
-    virtual ~SimpleServer();
+	protected:
+		void			registerSignal();
 
-    virtual void launch();
-    void setKeeprunning(bool keeprunning);
-    ListeningSocket * get_socket();
-  };
+	public:
+		SimpleServer(int domain, int service, int protocol, int port, u_long interface, int backlog);
+		virtual ~SimpleServer();
+
+		virtual void launch();
+		void setKeeprunning(bool keeprunning);
+		ListeningSocket * get_socket();
+};
 
 }
