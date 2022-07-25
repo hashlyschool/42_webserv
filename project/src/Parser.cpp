b5058a2e19ebb0208fbf6f84fee71f17ca474c2f
Gilliam6 <wizzzardoz@icloud.com>
2022-07-26 00:14:46 +0400

    Parser first version

--- project/src/Parser.cpp
+++ project/src/Parser.cpp
@@ -9,25 +9,69 @@ ft::Parser::Parser(std::string conf)
 		std::cerr << "Error can't open file" << std::endl;
 	try
 	{
-		char buf[10];
-		while (_fd.get(buf, 10, '\0'))
+		std::string buf;
+		while (std::getline(_fd, buf))
+		{
+			buf = trim(buf, " \t");
+			if (buf.empty() || buf[0] == '#')
+				continue;
 			_config += buf;
+			_config += "\n";
+		}
 	}
 	catch (std::ifstream::failure e) { std::cerr << "Ifstream failure exception" << std::endl; }
 	_fd.close();
 	Parse();
 }
 
-void ft::Parser::Parse() {
-	std::vector<std::string> token;
-	checkBrackets();
-	while (!_config.empty())
-		token.push_back(Split(_config, "\n"));
-	for (std::vector<std::string>::iterator it = token.begin(); it != token.end(); it++)
-		std::cout << *it << "\n";
-
+inline std::string ft::Parser::trim( std::string line, std::string trimmer)
+{
+	line.erase(line.find_last_not_of(trimmer)+1);         //suffixing spaces
+    line.erase(0, line.find_first_not_of(trimmer));       //prefixing spaces
+    return line;
 }
 
+void ft::Parser::Parse() {
+	std::vector<std::string> token;
+	std::string tmp;
+	checkBrackets();
+	while (!_config.empty())
+	{
+		tmp = Split(_config, "SERVER");
+		if (tmp.empty() == false)
+			token.push_back(tmp);
+	}
+//	for (std::vector<std::string>::iterator it = token.begin(); it != token.end(); it++)
+//		std::cout << *it << "\n";
+	int i = 3;
+	while (--i)
+		_configServers.push_back(new ConfigServer((parseOneServer(token[i]))));
+}
+
+ft::t_serverConf				ft::Parser::parseOneServer(std::string token)
+{
+	t_serverConf config;
+	std::string tmp;
+	config._port = "";
+	config._host = "";
+	config._serverName = "hi";
+	config._maxBodySize = 100;
+	while (!token.empty())
+	{
+		tmp = Split(token, "\n");
+		if (tmp.find("listen") != std::string::npos)
+		{
+			std::cout << tmp << "\n";
+			tmp = tmp.substr(7, 14);
+			std::cout << tmp << "\n";
+			config._host = tmp.substr(0, 9);
+			config._port = tmp.substr(10, 12);
+			std::cout << config._host << std::endl;
+			std::cout << config._port << std::endl;
+		}
+	}
+	return config;
+}
 void ft::Parser::checkBrackets()
 {
 	std::string::iterator begin = _config.begin();
@@ -42,8 +86,8 @@ void ft::Parser::checkBrackets()
 			bracket -= 1;
 		begin++;
 	}
-//	if (bracket != 0)
-	//	throw(std::exception());
+	if (bracket != 0)
+		throw(std::exception());
 }
 
 std::string ft::Parser::Split(std::string &line, std::string delimiter)
@@ -52,11 +96,15 @@ std::string ft::Parser::Split(std::string &line, std::string delimiter)
 	std::string token;
 	pos = line.find(delimiter);
 	if (pos == std::string::npos)
-		return (0);
+	{
+		token = line;
+		line.erase();
+		return (trim(token, " \t"));
+	}
 
     token = line.substr(0, pos);
     line.erase(0, pos + delimiter.length());
-	return (token);
+	return (trim(token, " \t"));
 }
 
 ft::Parser::~Parser() {
