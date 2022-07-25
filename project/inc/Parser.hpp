1065c0ac35f32d57c38479e0d165c427ef7fe9e3
Gilliam6 <wizzzardoz@icloud.com>
2022-07-25 22:23:14 +0400

    config Branch init commit

--- project/inc/Parser.hpp
+++ project/inc/Parser.hpp
@@ -1,26 +1,32 @@
-#pragma once
 
+#pragma once
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
 #include "./ConfigServer.hpp"
 
+namespace ft{
 
-namespace ft
-{
-
-	class Parser
-	{
+	class Parser {
 		private:
+		//	struct servers {
+		//		
+		//	};
 			std::vector<ConfigServer*>	_configServers;
-
 			t_serverConf				parseOneServer();
-			void						parseConfig(std::string pathConf);
-		public:
-			Parser(std::string pathConf);
-			virtual ~Parser();
+			
+			Parser();
+			std::string	Split(std::string &line, std::string delimiter);
+			void checkBrackets();
+			std::string _config;
 
-			//Getters
+		public:
+			Parser(std::string conf);
+			~Parser();
+			
 			int					getNumServers() const;
 			const ConfigServer*	getConfigServer(int index) const;
-
+			void Parse();
 	};
-
 }
